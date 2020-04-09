5ece426452d74a8b40301ca909e72072248087e9
Michael Zhukov <46239008+blablablajka@users.noreply.github.com>
2020-04-09 17:43:14 +0300

    Create Length of a vector with x, y, z coordinates.cpp

---
+++ Length of a vector with x, y, z coordinates.cpp
@@ -0,0 +1,13 @@
+#include<cmath>
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+
+using namespace std;
+
+
+int main(){
+ int x,y,z;
+ cin>>x>>y>>z;
+ cout<<sqrt((x*x)+(y*y)+(z*z));
+}
