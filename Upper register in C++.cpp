9c2d81b2b7918baf6c8755c84761c190ad0891dc
Michael Zhukov <46239008+blablablajka@users.noreply.github.com>
2020-04-09 13:19:47 +0300

    Update Upper register in C++.cpp

--- Upper register in C++.cpp
+++ Upper register in C++.cpp
@@ -10,3 +10,26 @@ int main(){
   std::transform(str.begin(), str.end(), str.begin(), (int (*) (int)) toupper);
   cout<<str;
 }
+/*
+Real Solution
+
+#include <iostream>
+ 
+int main()
+{
+char chLetter('a');
+  
+std::cout << "Введите букву нижнего регистра:\n\n";
+ 
+std::cin >> chLetter;
+ 
+chLetter = chLetter - 32;
+ 
+std::cout << "\nТа же буква в верхнем регистре: " << chLetter << ".\n\n";
+ 
+return 0;
+}
+
+
+
+*/
