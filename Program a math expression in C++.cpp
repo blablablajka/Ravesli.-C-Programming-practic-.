8b113b66058d5cb5fb0f6281d8800a141f6721e4
Michael Zhukov <46239008+blablablajka@users.noreply.github.com>
2020-04-09 12:54:16 +0300

    Create Program a math expression in C++.cpp

---
+++ Program a math expression in C++.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+ int a,b,f;
+ cout<<"a, b and f \n";
+ cout<<"a = ";
+ cin>>a;
+ cout<<"b = ";
+ cin>>b;
+ cout<<"f = ";
+ cin>>f;
+ cout<<"x = (a + b - f / a) + f * a * a - (a + b)"<<"\n";
+ cout<<(a + b - f / a) + f * a * a - (a + b);
+
+}
